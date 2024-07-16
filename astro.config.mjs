6773a928de241274f8e97f2ba4ba66168d59b009
kleinlau17 <kleinlau17@gmail.com>
2024-07-16 18:36:18 +0800

    启动端口修改

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,10 @@ import lit from "@astrojs/lit";
 
 // https://astro.build/config
 export default defineConfig({
+  server: {
+    host: '0.0.0.0',
+    port: 4321, // 确保端口设置正确
+  },
   site: 'https://odyssey-theme.sapling.supply/', // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
   sitemap: true, // Generate sitemap (set to "false" to disable)
   integrations: [sitemap(), mdx(), lit(), icon()], // Add renderers to the config
